e79761b0ab9e19cfedb94352e80e9949f696b3d4
Hugo Delgado <hugo.delgado@hiscox.com>
2024-01-16 20:22:36 +0000

    Final Products

--- LineTen/LineTen.Infrastructure/ProductRepository.cs
+++ LineTen/LineTen.Infrastructure/ProductRepository.cs
@@ -4,91 +4,88 @@ using LineTen.Infrastructure.Context;
 
 namespace LineTen.Infrastructure
 {
-  public class ProductRepository : IProductRepository
-  {
-    public static List<Product> lstProducts = new List<Product>()
+    public class ProductRepository : IProductRepository
+    {
+        public static List<Product> lstProducts = new List<Product>()
         {
-            new Product(1,"Name1", "Desc1", "1234567891" ),
-            new Product(2,"Name2", "Desc2", "1234567892" ),
-            new Product(3,"Name3", "Desc3", "1234567893" ),
-            new Product(4,"Name4", "Desc4", "1234567894" )
+            new Product(1,"Name1", "Desc1", "1234567891" )
         };
 
-    public Product CreateProducts(string name, string desc, string sku)
-    {
-      Product product;
-      try
-      {
-        using (var contxt = new LineTenContext())
+        public Product CreateProducts(string name, string desc, string sku)
         {
-          product = new Product(name, desc, sku);
+            Product product;
+            try
+            {
+                using (var contxt = new LineTenContext())
+                {
+                    product = new Product(name, desc, sku);
 
-          contxt.Products.Add(product);
-          contxt.SaveChanges();
+                    contxt.Products.Add(product);
+                    contxt.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return product;
         }
-      }
-      catch (Exception ex)
-      {
-        throw new Exception(ex.Message);
-      } 
-      return product;
+
+        public Product DeleteProducts(int id)
+        {
+            Product products;
+            using (var contxt = new LineTenContext())
+            {
+                products = contxt.Products.Where(x => x.Id == id).FirstOrDefault();
+
+                if (products != null)
+                {
+                    contxt.Products.Remove(products);
+                    contxt.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("No product found for the id.");
+                }
+            }
+            return products;
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> lst = new List<Product>();
+            using (var contxt = new LineTenContext())
+            {
+                var query = from productTable in contxt.Products
+                            orderby productTable.Id
+                            select productTable;
+
+                lst = query.ToList();
+
+            }
+            return lst;
+        }
+
+        public Product UpdateProducts(int id, string name, string desc, string sku)
+        {
+            Product products;
+            using (var contxt = new LineTenContext())
+            {
+                products = contxt.Products.Where(x => x.Id == id).FirstOrDefault();
+
+                if (products != null)
+                {
+                    products.Name = !string.IsNullOrEmpty(name) && products.Name != name ? name : products.Name;
+                    products.Description = !string.IsNullOrEmpty(desc) && products.Description != desc ? desc : products.Description;
+                    products.SKU = !string.IsNullOrEmpty(sku) && products.SKU != sku ? sku : products.SKU;
+                    contxt.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("No product found for the id.");
+                }
+            }
+            return products;
+        }
     }
-
-    public Product DeleteProducts(int id)
-    {
-      Product products;
-      using (var contxt = new LineTenContext())
-      {
-        products = contxt.Products.Where(x => x.Id == id).FirstOrDefault();
-
-        if (products != null)
-        {
-          contxt.Products.Remove(products);
-          contxt.SaveChanges();
-        }
-        else
-        {
-          throw new Exception("No product found for the id.");
-        }
-      }
-      return products;
-    }
-
-    public List<Product> GetProducts()
-    {
-      List<Product> lst;
-      using (var contxt = new LineTenContext())
-      {
-        var query = from productTable in contxt.Products
-                    orderby productTable.Id
-                    select productTable;
-
-        lst = query.ToList();
-
-      }
-      return lst;
-    }
-
-    public Product UpdateProducts(int id, string name, string desc, string sku)
-    {
-      Product products;
-      using (var contxt = new LineTenContext())
-      {
-        products = contxt.Products.Where(x => x.Id == id).FirstOrDefault();
-
-        if (products != null)
-        {
-          products.Name = !string.IsNullOrEmpty(name) && products.Name != name ? name : products.Name;
-          products.Description = !string.IsNullOrEmpty(desc) && products.Description != desc ? desc : products.Description;
-          products.SKU = !string.IsNullOrEmpty(sku) && products.SKU != sku ? sku : products.SKU;
-          contxt.SaveChanges();
-        }
-        else
-        {
-          throw new Exception("No product found for the id.");
-        }
-      }
-      return products;
-    }
-  }
 }
